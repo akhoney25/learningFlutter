32f77b0dec77c53ba7b99e9bd058f282725c4e11
Abdul Kareem <abdulkareemzahir9599@gmail.com>
2023-09-16 22:30:04 +0530

    day4 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:doctor_project/pages/home_page.dart';
 import 'package:doctor_project/pages/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main(){
   runApp(MyApp());
@@ -13,6 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
        themeMode: ThemeMode.light,
        theme: ThemeData(
+         fontFamily:GoogleFonts.lato().fontFamily,
          primarySwatch: Colors.deepOrange
        ),
         darkTheme: ThemeData(
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,18 +1,52 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
 
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text("login page",
-        style: TextStyle(
-          fontSize: 30,
-          color: Colors.red,
-          fontWeight: FontWeight.bold,
-         ),
-        ),
+      color: Colors.white,
+      child: Column(
+        children: [
+          Image.asset(
+              "assets/images/login.png",
+          fit: BoxFit.cover),
+          const SizedBox(
+            height: 25
+          ),
+          const Text("Welcome",
+              style: TextStyle(
+            fontSize: 25,
+              fontWeight: FontWeight.bold
+                 )
+            ),
+          const SizedBox(
+              height: 25
+          ),
+          Padding(
+              padding:
+                    const EdgeInsets.symmetric(vertical: 16.0,horizontal: 32.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                        hintText: "Enter Username",
+                        labelText: "Username"
+                    ),
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                        hintText: "Enter Password",
+                        labelText: "Password"
+                    ),
+                  ),
+                ],
+              ),
+          )
+        ],
       ),
     );
   }
